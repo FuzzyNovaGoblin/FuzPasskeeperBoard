b87c23cd3e44a3961373b81aa50df3f639a8d07f
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2021-12-12 15:44:05 -0500

    first attempt

--- arduino_keyboard_controller/src/main.cpp
+++ arduino_keyboard_controller/src/main.cpp
@@ -12,6 +12,7 @@ char getAsciiByte();
 void setup()
 {
   pinMode(activePin, INPUT);
+  // asm()
   pinMode(dataPin, INPUT);
   Keyboard.begin();
 }
@@ -19,27 +20,50 @@ void setup()
 void loop()
 {
   asciiByte = getAsciiByte();
+  Serial.println("here");
   Keyboard.print(asciiByte);
 }
 
 char getAsciiByte()
 {
-  char retByte = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    while (digitalRead(activePin) == LOW)
-    {
-      delay(1);
-    }
-    retByte = retByte << 1;
-    if (digitalRead(dataPin) == HIGH)
-    {
-      retByte = retByte | 1;
-    }
-    while (digitalRead(activePin) == HIGH)
-    {
-      delay(1);
-    }
-  }
-  return retByte;
+  Serial.println("here");
+
+  asm("CLR r16"); //char retByte = 0;
+  asm("CLR r17");
+  // for (int i = 0; i < 8; i++)
+  // {
+  asm("begin_loop:inc r17\n");
+
+  asm(
+      "p5loop_start:\n"
+      "sbis %0, %1\n"
+      "jmp p5loop_start" ::"I"(_SFR_IO_ADDR(PINC)),
+      "I"(PINC6));
+  asm("LSL r16");
+
+  asm(
+      "p5loop:\n"
+      "sbis %0, %1\n"
+      "jmp after_bit_set\n"
+      "jmp is_high_pin_6\n" ::"I"(_SFR_IO_ADDR(PIND)),
+      "I"(PIND7));
+  asm("is_high_pin_6:\n");
+  asm("ori r16, 0x1");
+  asm("after_bit_set:\n");
+
+  asm(
+      "p5loop_end:\n"
+      "sbis %0, %1\n"
+      "jmp p5loop_end" ::"I"(_SFR_IO_ADDR(PINC)),
+      "I"(PINC6));
+  char retbyte = 0;
+  Serial.println((int)retbyte);
+
+  asm("cpi r17, 0x8\n"
+      "brlo begin_loop\n");
+
+  // char retbyte = 0;
+  asm("mov %0, r16" : "=r"(retbyte));
+  Serial.println((int)retbyte);
+  return retbyte;
 }
